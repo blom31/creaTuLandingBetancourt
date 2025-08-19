095d0b0127e64efec83436a5ec14bd6b84e64f88
Olga Betancourt <blom31@gmail.com>
2025-08-19 19:47:31 -0300

    corrección de ruta en fichero orderService.js

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,5 +1,5 @@
 import { doc, setDoc } from "firebase/firestore";
-import { db } from "../../firebase/firebaseConfig";
+import { db } from "../firebase/firebaseConfig";
 
 export const createOrderObject = (user, cart, total) => ({
   buyer: {
